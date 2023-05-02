04b068d0059e2f587a73997ef901aaa2acca54d8
Parham Kouloubandi <122618163+datenlabor01@users.noreply.github.com>
2023-05-02 10:44:29 +0200

    Update Landextract.py

--- Landextract.py
+++ Landextract.py
@@ -4,27 +4,36 @@ pd.options.mode.chained_assignment = None
 from dash import Dash, html, dcc, Input, Output, dash_table, State
 import dash_bootstrap_components as dbc
 
-df_data = pd.read_csv("https://raw.githubusercontent.com/datenlabor01/Land_Extractor/main/gv_data.csv")
+#df_data = pd.read_csv("https://raw.githubusercontent.com/datenlabor01/Land_Extractor/main/gv_data.csv")
+df_data = pd.read_excel("https://github.com/datenlabor01/Land_Extractor/blob/main/gv_data.xlsx?raw=true")
+df_data["Land"] = df_data["Land"].astype(str)
+df_data.Projektbeginn = df_data.Projektbeginn.astype(str).str[:10]
+df_data["Geplantes Projektende"] = df_data["Geplantes Projektende"].astype(str).str[:10]
+
+df_data = df_data.rename(columns={"Geplante Beauftragungen 2023": "Geplante Beauftragungen gesamt 2023"})
+
 app = Dash(external_stylesheets = [dbc.themes.LUX])
+
+formatted = {'locale': {'decimal': ',', 'symbol': ['', '€'], 'group': '.'}, 'nully': '', 'prefix': None, 'specifier': '$,.2f'}
 
 #Dropdown for indicator:
 project_select = dcc.Dropdown(options=sorted(df_data["BMZ Nummer"].unique()),
                              value='All', style={"textAlign": "center"}, clearable=False, multi=True, placeholder='Projektnummer auswählen')
 
 value_select = dcc.Dropdown(options=["Auftragsbetrag gesamt BMZ-Mittel", "Auszahlungsbetrag gesamt",
-                                     "Geplante Beauftragungen 2023"],
+                                     "Geplante Beauftragungen gesamt 2023"],
                              value='All', style={"textAlign": "center"}, clearable=False, multi=False, placeholder='Messgröße auswählen (Default: Auftragsbetrag BMZ-Mittel)')
 
-country_select = dcc.Dropdown(options=sorted(df_data["Country"].unique()),
-                             value='All', style={"textAlign": "center"}, clearable=False, multi=True, placeholder='Land auswählen')
+country_select = dcc.Dropdown(id="country_dd", options=sorted(df_data["Land"].unique()),
+                             value=[], style={"textAlign": "center"}, clearable=False, multi=True, placeholder='Land auswählen')
 
 text2 = "Diese Anwendung wird als Prototyp vom BMZ Datenlabor angeboten. Sie kann Fehler enthalten und ist als alleinige Entscheidungsgrundlage nicht geeignet. Außerdem können Prototypen ausfallen oder kurzfristig von uns verändert werden. Sichern Sie daher wichtige Ergebnisse per Screenshot oder Export. Die Anwendung ist vorerst intern und sollte daher nicht ohne Freigabe geteilt werden. Wenden Sie sich bei Fragen gerne an datenlabor@bmz.bund.de"
 
 app.layout = dbc.Container([
       dbc.Row([
          html.Div(html.Img(src="https://github.com/datenlabor01/LS/raw/main/logo.png", style={'height':'80%', 'width':'20%'})),
-         html.H1(children='Prototyp Länderüberblick'),
-         html.P(children = "Das ist ein Prototyp, der Fehler enthalten kann.")],
+         html.H1(children='Prototyp Überblick Globalvorhaben'),
+         html.P(children = "Das ist ein Prototyp, der Fehler enthalten kann. Es zeigt für ausgewählte Vorhaben die Partnerländer, die in der GIZ-Projektdatenbank erwähnt werden. Nur Vorhaben mit Partnerland werden in der Karte angezeigt. Bei Klick auf ein Land in der Karte werden die Länder angezeigt, in denen Vorhaben mit dem ausgewählten Land stattfinden.")],
          style={'textAlign': 'center'}),
 
       dbc.Row([
@@ -38,18 +47,12 @@ app.layout = dbc.Container([
       ], justify = "center"),
 
       dbc.Row([
-        dbc.Col([project_select, html.Br(), value_select], width = 8),
-      ], justify = "center"),
+        dbc.Col([project_select, html.Br(), value_select, html.Br(), country_select], width = 8)],
+        justify = "center"),
 
       dbc.Row([
          dcc.Graph(id='map', style={'textAlign': 'center'}),
       ]),
-
-      dbc.Row([
-        dbc.Col([
-         dcc.Dropdown(id="country_dd", placeholder='Alle Länder', value = "All",
-         style = {"textAlign": "center"}, clearable=True, multi=True), html.Br()], width = 8),
-      ], justify = "center"),
 
       #Data Table:
       dbc.Row([
@@ -62,7 +65,7 @@ app.layout = dbc.Container([
             'if': {'row_index': 'odd'},
             'backgroundColor': 'rgb(235, 240, 240)',
         }], export_format= "xlsx"),
-         ]),
+         ], style={"margin-bottom": "80px"}),
 ])
 
 #Button to display text:
@@ -83,46 +86,58 @@ def collapse(n, is_open):
     Input(project_select, 'value'),
     )
 
-def country_options(project_select ):
+def country_options(project_select):
     if (project_select == "All") | (project_select == []):
-      return sorted(df_data['Country'].unique())
+      return sorted(df_data['Land'].unique())
     else:
       df_temp = df_data[df_data["BMZ Nummer"].isin(project_select)]
-      return sorted(df_temp['Country'].unique())
+      return sorted(df_temp['Land'].unique())
 
 @app.callback(
-    [Output('map', 'figure'), Output(my_table, "data"), Output(my_table, "columns")],
-    [Input(project_select, 'value'), Input(value_select, 'value'), Input("country_dd", 'value')]
+    [Output('map', 'clickData'), Output("country_dd", 'value'), Output('map', 'figure'), Output(my_table, "data"), Output(my_table, "columns")],
+    [Input('map', 'clickData'), Input(project_select, 'value'), Input(value_select, 'value'), Input("country_dd", 'value')],
+    prevent_initial_call=True
 )
 
-def update_map(project_select, value_select, country_select):
+def update_map(clickData, project_select, value_select, country_select):
+
+   if clickData:
+        country = clickData['points'][0]['hovertext']
+        if country in country_select:
+            country_select.remove(country)
+        else:
+            country_select.append(country)
 
    if (project_select == "All") | (project_select == []):
-      dat_map = df_data
+       dat_map = df_data
    else:
-      dat_map = df_data[df_data["BMZ Nummer"].isin(project_select)]
+       dat_map = df_data[df_data["BMZ Nummer"].isin(project_select)]
 
    if (value_select == "All") | (value_select == "Auftragsbetrag gesamt BMZ-Mittel"):
       dat_map["Value"]  = dat_map["Auftragsbetrag gesamt BMZ-Mittel"]
    if value_select == "Auszahlungsbetrag gesamt":
       dat_map["Value"] = dat_map["Auszahlungsbetrag gesamt"]
-   if value_select == "Geplante Beauftragungen 2023":
-      dat_map["Value"] = dat_map["Geplante Beauftragungen 2023"]
+   if value_select == "Geplante Beauftragungen gesamt 2023":
+      dat_map["Value"] = dat_map["Geplante Beauftragungen gesamt 2023"]
+
+   if (country_select == []) | (not country_select):
+       dat_map = dat_map
+   else:
+       dat_map = dat_map[dat_map["BMZ Nummer"].isin(dat_map[dat_map["Land"].isin(country_select)]["BMZ Nummer"])]
+       #dat_map = dat_map[dat_map["Land"].isin(country_select)]
 
    dat = dat_map.groupby(["Land", "Country"])["Value"].sum().reset_index()
 
-   figMap = px.choropleth(dat, locations = "Land", locationmode="ISO-3", hover_data= ["Country"],
-                        color_continuous_scale="Fall", color= "Value")
+   figMap = px.choropleth(dat, locations = "Country", locationmode="ISO-3", hover_name= "Land", hover_data= ["Land", "Country"],
+                        color_continuous_scale="Fall", color= "Value", projection="natural earth")
    figMap.update(layout_coloraxis_showscale = False)
+   figMap.update_layout(autosize=False, height=600)
 
-   if (country_select == "All") | (country_select == []):
-      dat_table = dat_map
-   else:
-      dat_table = dat_map[dat_map["Country"].isin(country_select)]
+   dat_table = dat_map.drop_duplicates(subset=["BMZ Nummer"])
 
-   dat_table = dat_table.drop_duplicates(subset=["BMZ Nummer"])
+   columns = [{"name": i, "id": i, "type": "numeric", "format": formatted} if "gesamt" in i else {"name": i, "id": i} for i in dat_table.columns[3:-1]]
 
-   return figMap, dat_table.to_dict("records"), [{"name": i, "id": i} for i in dat_table.columns[3:-1]]
+   return None, country_select, figMap, dat_table.to_dict("records"), columns
 
 if __name__ == '__main__':
     app.run_server(debug=True)
